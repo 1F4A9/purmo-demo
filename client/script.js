1d4498edf6c169a1b48f4f70eb4880055c3b036b
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-10-28 09:39:23 +0100

    c

--- client/script.js
+++ client/script.js
@@ -23,21 +23,16 @@ const config = {
 let categoryId;
 let productId;
 
-axios
-  .post(
-    "https://purmo-dop.webservice.pl/src//ajax/get-data.php?",
-    "siteid=2&mode=1",
-    config
-  )
-  .then((response) => {
-    console.log(response.data.content.categories);
-    response.data.content.categories.forEach((radiator) => {
+axios.get('/resources/categories')
+  .then(res => res.data)
+  .then(data => {
+    data.forEach((radiator) => {
       const option = document.createElement("option");
-      option.setAttribute("value", radiator.id);
+      option.setAttribute("value", radiator.categoryid);
       option.textContent = radiator.name;
       selectCategory.appendChild(option);
     });
-  });
+  })
 
 selectCategory.addEventListener("change", (e) => {
   selectProduct.removeAttribute("disabled");
@@ -46,53 +41,142 @@ selectCategory.addEventListener("change", (e) => {
     selectProduct.removeChild(selectProduct.firstChild);
   }
 
-  categoryId = e.target.value;
-  axios
-    .post(
-      "https://purmo-dop.webservice.pl/src//ajax/get-data.php?",
-      `siteid=2&mode=1&categoryid=${categoryId}`,
-      config
-    )
-    .then((response) => {
-      console.log(response.data.content.products);
+  axios.get(`/resources/products/${e.target.value}`)
+    .then(res => res.data)
+    .then(data => {
       const option = document.createElement("option");
       option.textContent = "Choose a model ...";
       selectProduct.appendChild(option);
 
-      response.data.content.products.forEach((product) => {
+      data.forEach((product) => {
         const option = document.createElement("option");
-        option.setAttribute("value", product.id);
+        option.setAttribute("value", product.productid);
         option.textContent = product.name;
         selectProduct.appendChild(option);
       });
-    });
-});
+    })
+})
 
 selectProduct.addEventListener("change", (e) => {
   selectType.removeAttribute("disabled");
+
+  console.log(e)
 
   while (selectType.firstChild) {
     selectType.removeChild(selectType.firstChild);
   }
 
-  productId = e.target.value;
-  axios
-    .post(
-      "https://purmo-dop.webservice.pl/src//ajax/get-data.php?",
-      `siteid=2&mode=1&categoryid=${categoryId}&productid=${productId}`,
-      config
-    )
-    .then((response) => {
-      console.log(response.data.content.types);
+  axios.get(`/resources/types/${e.target.value}`)
+    .then(res => res.data)
+    .then(data => {
       const option = document.createElement("option");
-      option.textContent = "Choose the type ...";
+      option.textContent = "Choose a type ...";
       selectType.appendChild(option);
 
-      response.data.content.types.forEach((type) => {
+      data.forEach((type) => {
         const option = document.createElement("option");
-        option.setAttribute("value", type.id);
+        option.setAttribute("value", type.typeid);
         option.textContent = type.name;
         selectType.appendChild(option);
       });
-    });
-});
+    })
+})
+
+selectProduct.addEventListener("change", (e) => {
+  selectType.removeAttribute("disabled");
+
+  console.log(e)
+
+  while (selectType.firstChild) {
+    selectType.removeChild(selectType.firstChild);
+  }
+
+  axios.get(`/resources/types/${e.target.value}`)
+    .then(res => res.data)
+    .then(data => {
+      const option = document.createElement("option");
+      option.textContent = "Choose a type ...";
+      selectType.appendChild(option);
+
+      data.forEach((type) => {
+        const option = document.createElement("option");
+        option.setAttribute("value", type.typeid);
+        option.textContent = type.name;
+        selectType.appendChild(option);
+      });
+    })
+})
+
+// axios
+//   .post(
+//     "https://purmo-dop.webservice.pl/src//ajax/get-data.php?",
+//     "siteid=2&mode=1",
+//     config
+//   )
+//   .then((response) => {
+//     console.log(response.data.content.categories);
+//     response.data.content.categories.forEach((radiator) => {
+//       const option = document.createElement("option");
+//       option.setAttribute("value", radiator.id);
+//       option.textContent = radiator.name;
+//       selectCategory.appendChild(option);
+//     });
+//   });
+
+// selectCategory.addEventListener("change", (e) => {
+//   selectProduct.removeAttribute("disabled");
+
+//   while (selectProduct.firstChild) {
+//     selectProduct.removeChild(selectProduct.firstChild);
+//   }
+
+//   categoryId = e.target.value;
+//   axios
+//     .post(
+//       "https://purmo-dop.webservice.pl/src//ajax/get-data.php?",
+//       `siteid=2&mode=1&categoryid=${categoryId}`,
+//       config
+//     )
+//     .then((response) => {
+//       console.log(response.data.content.products);
+//       const option = document.createElement("option");
+//       option.textContent = "Choose a model ...";
+//       selectProduct.appendChild(option);
+
+//       response.data.content.products.forEach((product) => {
+//         const option = document.createElement("option");
+//         option.setAttribute("value", product.id);
+//         option.textContent = product.name;
+//         selectProduct.appendChild(option);
+//       });
+//     });
+// });
+
+// selectProduct.addEventListener("change", (e) => {
+//   selectType.removeAttribute("disabled");
+
+//   while (selectType.firstChild) {
+//     selectType.removeChild(selectType.firstChild);
+//   }
+
+//   productId = e.target.value;
+//   axios
+//     .post(
+//       "https://purmo-dop.webservice.pl/src//ajax/get-data.php?",
+//       `siteid=2&mode=1&categoryid=${categoryId}&productid=${productId}`,
+//       config
+//     )
+//     .then((response) => {
+//       console.log(response.data.content.types);
+//       const option = document.createElement("option");
+//       option.textContent = "Choose the type ...";
+//       selectType.appendChild(option);
+
+//       response.data.content.types.forEach((type) => {
+//         const option = document.createElement("option");
+//         option.setAttribute("value", type.id);
+//         option.textContent = type.name;
+//         selectType.appendChild(option);
+//       });
+//     });
+// });
